08ce18be8df3c30551a13ba6266c122b72e9cfeb
npt306 <nptai212@clc.fitus.edu.vn>
2024-10-24 09:00:18 +0700

    validate client parameters - datatype

--- src/films/dto/create-film.dto.ts
+++ src/films/dto/create-film.dto.ts
@@ -1,15 +1,42 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateFilmDto {
+    @IsNumber()
     film_id: number;
+
+    @IsString()
     title: string;
+
+    @IsString()
     description: string;
+
+    @IsString()
     release_year: string;
+
+    @IsNumber()
     language_id: number;
+
+    @IsNumber()
     original_language_id: number;
-    retal_duration: number;
+
+    @IsNumber()
+    rental_duration: number;
+
+    @IsNumber()
     rental_rate: number;
+
+    @IsNumber()
     length: number;
+
+    @IsNumber()
     replacement_cost: number;
+
+    @IsString()
     rating: string;
-    speacial_features: string;
+
+    @IsString()
+    special_features: string;
+
+    @IsString()
     last_update: Date;
 }
